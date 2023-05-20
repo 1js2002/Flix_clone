1433b518c8947f4799a7f605a95e2525ef6c8e1f
1js2002 <dgoud99120@gmail.com>
2023-05-20 18:11:25 +0530

    building nav bar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5jlyiLZ6lI_c2NOaYnG2WeQHkBGp1zmA",
+    authDomain: "netflix-clone-e822a.firebaseapp.com",
+    projectId: "netflix-clone-e822a",
+    storageBucket: "netflix-clone-e822a.appspot.com",
+    messagingSenderId: "734411744185",
+    appId: "1:734411744185:web:932468555931723d280c68"
+  };
