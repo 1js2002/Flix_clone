d0e5d355827ad600ec0e583ef14d6bf23d9c3529
1js2002 <dgoud99120@gmail.com>
2023-05-23 12:58:23 +0530

    building row component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import './App.css';
 import HomeScreen from './HomeScreen';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
       <HomeScreen />
     </div>
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,26 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./Requests";
 
 function HomeScreen() {
   return (
     <div className="Homescreen">
         <Nav/>
         <Banner />
+
+        <Row 
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow />
+        <Row title="Trending Now" fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
